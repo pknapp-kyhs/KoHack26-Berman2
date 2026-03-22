6553388523f39e1e360e7f1a673291bd442f2945
= <=>
2026-03-22 16:59:15 -0400

    added more things

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ function createKey(value = ''){
         return createKey(value+digits.split("")[Math.floor(Math.random() * digits.length)]);
     }
     else{
-        let data = fs.readFileSync('accounts.json');
+        let data = JSON.parse(fs.readFileSync('accounts.json'));
         for(let i = 0; i < data.length; i++){
             //handles an absurdly unlikely edge case of two randomly generated keys being identical
             if(data[i].loginKey === value){
@@ -26,16 +26,10 @@ app.get('/',(req,res)=>{
     res.type('text/html');
     res.sendFile(path.join(__dirname,'main',"main.html"));
 })
+//sends users to login page
 app.get('/login',(req, res)=>{
     res.type("text/html");
     res.sendFile(path.join(__dirname, 'login','login.html'));
-});
-//sends users to the login page
-
-//unfinished get request, will require authenticated key to use
-app.get('/addRating/:key',()=>{
-    res.type('text/html');
-    res.sendFile(path.join(__dirname,'addrating','addrating.html'));
 });
 //specifically for the key, must be last get request or the whole thing breaks
 app.get('/:key', (req, res) => {
@@ -51,12 +45,53 @@ app.get('/:key', (req, res) => {
     }
 });
 //post request to add a review
-app.post('/AddReview',(req,res)=>{
+app.post('/addReview',(req,res)=>{
+    //error handling
+    if(!req.body.key){
+        res.send(new Error('key not valid'));
+    }
+    //reads accounts.json file
+    let data = JSON.parse(fs.readFile('accounts.json'));
+    for(let i = 0; i < data.length; i++){
+        //checks if loginKey of a particular account is 
+        if(data[i].loginKey === req.body.key){
+            fs.readFile('reviews.json',(err, dat)=>{
+                if(err){
+                    throw new Error(err);
+                }
+                let parsedData = JSON.parse(dat);
+                for(let x = 0; x < parsedData.shuls.length; x++){
+                        if(parsedData.shuls[x].name === req.body.name){
+                            parsedData.shuls[x].reviews.push({
+                                text:req.body.text,
+                                rating:req.body.rating,
+                                category:req.body.category,
+                                author:data[i].username,
+                        })
+                    }
+                }
+                fs.writeFile('reviews.json');
+
+            })
+        }
+    }
 });
+app.post('/addShul',(req,res)=>{
+    let body = req.body;
+    let data = JSON.parse(fs.readFileSync('accounts.json'));
+    data.shuls.push({
+        name:body.name,
+        lat:body.lat,
+        lng:body.lng,
+        location:body.location,
+        reviews:[]
+    })
+    res.send('done');
+})
 //post request to sign in (data validation)
 app.post('/signin',(req,res)=>{
     let key;
-    let data = fs.readFileSync('accounts.json');
+        let data = fs.readFileSync('accounts.json');
     for(let i = 0; i < data;i++){
         if(data[i].username === req.body.username && data[i].passcode === req.body.passcode){
             key = createKey();
